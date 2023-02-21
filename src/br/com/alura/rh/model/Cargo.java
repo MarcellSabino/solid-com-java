c34605f2bdd51dec32086084e6b05c331b13d87f
Marcell Sabino <sabino.marcell@gmail.com>
2023-02-21 10:09:27 -0300

    Criado servico de promocao e metodo abstrato no enum CARGO

--- src/br/com/alura/rh/model/Cargo.java
+++ src/br/com/alura/rh/model/Cargo.java
@@ -2,9 +2,23 @@ package br.com.alura.rh.model;
 
 public enum Cargo {
 
-	ASSISTENTE,
-	ANALISTA,
-	ESPECIALISTA,
-	GERENTE;
+	ASSISTENTE {
+		@Override
+		public Cargo getNovoCargo() { return ANALISTA; }
+	},
+	ANALISTA {
+		@Override
+		public Cargo getNovoCargo() { return ESPECIALISTA; }
+	},
+	ESPECIALISTA {
+		@Override
+		public Cargo getNovoCargo() { return GERENTE; }
+	},
+	GERENTE {
+		@Override
+		public Cargo getNovoCargo() { return GERENTE; }
+	};
+
+	public abstract Cargo getNovoCargo();
 
 }
---
+++ src/br/com/alura/rh/service/promocao/PromocaoService.java
@@ -0,0 +1,27 @@
+package br.com.alura.rh.service.promocao;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Cargo;
+import br.com.alura.rh.model.Funcionario;
+
+public class PromocaoService {
+	
+	public void promover (Funcionario funcionario , boolean metaBatida) {
+		
+		Cargo cargoAtual = funcionario.getCargo();
+		
+		if(cargoAtual == Cargo.GERENTE) {
+			throw new ValidacaoException("Gerente não pode ser promovido");
+		}
+		
+		if (metaBatida) {
+			
+			Cargo novoCargo = cargoAtual.getNovoCargo();
+			funcionario.setCargo(novoCargo);
+
+		}
+		
+	}
+	
+	
+}
